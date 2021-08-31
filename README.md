1223198f1c274deeef56cc24bdac7c498e96f210
Jean Carlo Leon <jeancarloleonpr@gmail.com>
2021-08-31 16:03:52 +0200

    step 19 included

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
+
+# Introduction
+SAPUI5 Projet from 0 using the walkthrough example
+We created all folders and files without any toolkit or template
+
+```
+put some code here!
+```
+
 # Component-preload (Error in console)
 Essentially the component preload concatenates all the files used by the component, 
 that is all the controllers, views, formatters etc into a single file and minifies the js, 
 json and xml code. 
 The idea being that 1 minified file for all you components is loaded a lot quicker 
 than many un-minified files. Its very effective at reducing the loading of applications.
-
-```
-out some code here
-```
+To avoid the local test error on the chrome console, we create a file to detect the existence of the file
+but when we "build" the project to deploy it (ui5 build), we can apreciate the file on the dist folder.
--- webapp/index.js
+++ webapp/index.js
@@ -1,27 +1,3 @@
-// We dont need any more this due we call the view
-// sap.ui.define([
-//     "sap/m/Text"
-// ], function( Text ) {
-//     'use strict';
-//     new Text({
-//         text: "Hello UI5!"
-//     }).placeAt("content");
-//     //console.log("UI is ready to go!");
-// });
-
-// sap.ui.define([
-// 	"sap/ui/core/mvc/XMLView"
-// ], function (XMLView) {
-// 	"use strict";
-
-// 	XMLView.create({
-// 		viewName: "sap.ui.demo.walkthrough.view.App"
-// 	}).then(function (oView) {
-// 		oView.placeAt("content");
-// 	});
-
-// });
-
 sap.ui.define([
 	"sap/ui/core/ComponentContainer"
 ], function (ComponentContainer) {
@@ -35,20 +11,3 @@ sap.ui.define([
 		async: true
 	}).placeAt("content");
 });
-
-//************** ALTERNATIVA ojo no todos los navegadores soportaran ES6 en nuestro caso debemos 
-// analizar el proyecto si será desplegado en navegadores actualizados o trabajaremos con versiones anteriores 
-// por lo cual no podremos hacer uso de ES6 ********************** */
-
-// const arrControl = [
-//     "sap/m/Text"
-// ];
-
-// const initSAPUI5 = ( Text ) => { 
-//     new Text({
-//         text: "Hello UI5!"
-//     }).placeAt("content");
-// };
-
-// sap.ui.define( arrControl, initSAPUI5 );
-
